45780a2186a2014515e459c1d8dd66a6d3422b88
Ana Barros <up201806593@fe.up.pt>
2021-03-19 10:38:26 +0000

    Fix: rmi stub

--- proj1/src/Peer.java
+++ proj1/src/Peer.java
@@ -9,80 +9,83 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Peer implements RMI {
-  private int id;
-  private String version, access_point;
-  private MC_Channel mc_channel;
-  private MDB_Channel mdb_channel;
-  private MDR_Channel mdr_channel;
-  private Storage storage;
+    private int id;
+    private String version, access_point;
+    private MC_Channel mc_channel;
+    private MDB_Channel mdb_channel;
+    private MDR_Channel mdr_channel;
+    private Storage storage;
 
-  public static void main(String[] args) {
-    if(args.length != 9){
-      usage();
-      System.exit(1);
+    public static void main(String[] args) {
+        if (args.length != 9) {
+            usage();
+            System.exit(1);
+        }
+
+        Peer peer_obj = new Peer(args);
+        try { //RMI
+            RMI RMI_stub = (RMI) UnicastRemoteObject.exportObject(peer_obj, 0);
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind(peer_obj.access_point, RMI_stub);
+        } catch (RemoteException | AlreadyBoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    Peer peer_obj = new Peer(args);
-    try { //RMI
-      RMI RMI_stub = (RMI) UnicastRemoteObject.exportObject(peer_obj,0);
-      // Bind the remote object's stub in the registry
-      Registry registry = LocateRegistry.getRegistry();
-      registry.bind(peer_obj.access_point, RMI_stub);
-    } catch (RemoteException | AlreadyBoundException e) {
-      e.printStackTrace();
+    public Peer(String[] args) {
+        this.version = args[0];
+        try {
+            id = Integer.parseInt(args[1]);
+            access_point = args[2];
+            mc_channel = new MC_Channel(args[3], Integer.parseInt(args[4]));
+            mdb_channel = new MDB_Channel(args[5], Integer.parseInt(args[6]));
+            mdr_channel = new MDR_Channel(args[7], Integer.parseInt(args[8]));
+
+        } catch (NumberFormatException e) {
+            System.out.println("Exception: " + e.getMessage());
+            System.exit(1);
+        }
+        storage = Storage.getInstance();
+        storage.makeDirectories(id);
     }
 
-  }
-
-  public Peer(String[] args){
-    this.version = args[0];
-    try {
-      id = Integer.parseInt(args[1]);
-      access_point = args[2];
-      mc_channel = new MC_Channel(args[3], Integer.parseInt(args[4]));
-      mdb_channel = new MDB_Channel(args[5], Integer.parseInt(args[6]));
-      mdr_channel = new MDR_Channel(args[7], Integer.parseInt(args[8]));
-
-    }catch (NumberFormatException e){
-      System.out.println("Exception: " + e.getMessage());
-      System.exit(1);
+    private static void usage() {
+        System.out.println("Usage: <protocol version> <peer ID> <service access point> <MC> <MDB> <MDR>");
     }
-    storage = Storage.getInstance();
-    storage.makeDirectories(id);
-  }
 
-  private static void usage(){
-    System.out.println("Usage: <protocol version> <peer ID> <service access point> <MC> <MDB> <MDR>");
-  }
+    @Override
+    public void backupFile(String file_pathname, int replication_degree) {
+        File file = storage.getFile(file_pathname);
 
-  @Override
-  public void backupFile(String file_pathname, int replication_degree) {
-    File file = storage.getFile(file_pathname);
-    if(file == null){
-      System.out.println("File does not exist. Aborting.");
-    }else {
-      storage.addBackedUpFile(file.toPath());
-      new Backup(this.id, file, replication_degree);
+        if (file == null) {
+            System.out.println("File does not exist. Aborting.");
+        }
+
+        else {
+            storage.addBackedUpFile(file.toPath());
+            new Backup(this.id, file, replication_degree);
+        }
     }
-  }
 
-  @Override
-  public void restoreFile(String file_pathname) {
-    System.out.println("Not implemented yet");
-  }
+    @Override
+    public void restoreFile(String file_pathname) {
+        System.out.println("Not implemented yet");
+    }
 
-  @Override
-  public void deleteFile(String file_pathname) {
-    System.out.println("Not implemented yet");
-  }
+    @Override
+    public void deleteFile(String file_pathname) {
+        System.out.println("Not implemented yet");
+    }
 
-  @Override
-  public void reclaim (int max_space){
-    System.out.println("Not implemented yet");
-  }
+    @Override
+    public void reclaim(int max_space) {
+        System.out.println("Not implemented yet");
+    }
 
-  @Override
-  public void getStateInformation() {
-    System.out.println("Not implemented yet");
-  }
+    @Override
+    public void getStateInformation() {
+        System.out.println("Not implemented yet");
+    }
 }
--- proj1/src/TestApp.java
+++ proj1/src/TestApp.java
@@ -3,63 +3,76 @@ import java.rmi.RemoteException;
 import java.rmi.registry.*;
 
 public class TestApp {
-  public static void main(String[] args) {
-    if(args.length < 2){
-      usage();
-      System.exit(1);
+    public static void main(String[] args) throws RemoteException {
+        if (args.length < 2) {
+            usage();
+            System.exit(1);
+        }
+
+        Registry registry = null;
+        RMI stub = null;
+        try {
+            registry = LocateRegistry.getRegistry(); // default port: 1099
+            stub = (RMI) registry.lookup(args[0]);
+        } catch (RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+
+        switch (args[1]) {
+            case "BACKUP":
+                if (args.length < 4) {
+                    usage();
+                    System.exit(1);
+                }
+
+                stub.backupFile(args[2], Integer.parseInt(args[3]));
+
+                break;
+
+            case "RESTORE":
+                if (args.length < 3) {
+                    usage();
+                    System.exit(1);
+                }
+
+                stub.restoreFile(args[2]);
+
+                break;
+
+            case "DELETE":
+                if (args.length < 3) {
+                    usage();
+                    System.exit(1);
+                }
+
+                stub.deleteFile(args[2]);
+
+                break;
+
+            case "RECLAIM":
+                if (args.length < 3) {
+                    usage();
+                    System.exit(1);
+                }
+
+                stub.reclaim(Integer.parseInt(args[2]));
+
+                break;
+
+            case "STATE":
+                stub.getStateInformation();
+
+                break;
+
+            default:
+                System.out.println("Unknown operation.\n");
+                usage();
+                System.exit(1);
+        }
     }
 
-    Registry registry = null;
-    Peer peer_stub = null;
-    try {
-      registry = LocateRegistry.getRegistry(); // default port: 1099
-      peer_stub = (Peer) registry.lookup(args[0]);
-    } catch (RemoteException | NotBoundException e) {
-      e.printStackTrace();
+    private static void usage() {
+        System.out.println("usage: java TestApp <peer_ap> <sub_protocol> <opnd_1> <opnd_2>");
     }
-
-    switch (args[1]){
-      case "BACKUP":
-        if(args.length < 4){
-          usage();
-          System.exit(1);
-        }
-        peer_stub.backupFile(args[2], Integer.parseInt(args[3]));
-        break;
-
-      case "RESTORE":
-        if(args.length < 3){
-          usage();
-          System.exit(1);
-        }
-        peer_stub.restoreFile(args[2]);
-        break;
-      case "DELETE":
-        if(args.length < 3){
-          usage();
-          System.exit(1);
-        }
-        peer_stub.deleteFile(args[2]);
-        break;
-      case "RECLAIM":
-        if(args.length < 3){
-          usage();
-          System.exit(1);
-        }
-        peer_stub.reclaim(Integer.parseInt(args[2]));
-        break;
-      case "STATE":
-        peer_stub.getStateInformation();
-        break;
-      default:
-        System.out.println("Unknown operation.\n");
-        usage();
-        System.exit(1);
-    }
-  }
-
-  private static void usage(){
-    System.out.println("usage: java TestApp <peer_ap> <sub_protocol> <opnd_1> <opnd_2>");
-  }
 }
 
