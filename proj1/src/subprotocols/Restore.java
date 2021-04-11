d8b412066c27c5fb4c8e21e0b8c5ea93291b943f
Educorreia932 <skelozard1@gmail.com>
2021-04-11 15:05:36 +0100

    Using AsynchronousFileChannels in Restore

--- proj1/src/subprotocols/Restore.java
+++ proj1/src/subprotocols/Restore.java
@@ -4,11 +4,18 @@ import channels.MC_Channel;
 import channels.MDR_Channel;
 import messages.GetChunkMessage;
 import peer.Peer;
+import peer.storage.Storage;
 import utils.Pair;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class Restore extends Subprotocol {
     private final MDR_Channel restore_channel;
@@ -35,7 +42,7 @@ public class Restore extends Subprotocol {
         while (chunk_no < number_of_chunks) {
             // Send message
             control_channel.send(message.getBytes(null, 0));
-            System.out.printf("< Peer %d sent: %s\n", initiator_peer.id, message.toString());
+            System.out.printf("< Peer %d sent: %s\n", initiator_peer.id, message);
 
             try {
                 Thread.sleep(500);
@@ -46,6 +53,7 @@ public class Restore extends Subprotocol {
                 if (has_chunk) // Chunk received => Skip to next chunk
                     message.setChunkNo(++chunk_no);
             }
+
             catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -57,24 +65,38 @@ public class Restore extends Subprotocol {
             System.out.println("Failed to restore files of " + file_path);
     }
 
+    /**
+     * Write restored file chunks
+     */
     private boolean restoreFileChunks() {
         ArrayList<byte[]> chunks = new ArrayList<>(); // Array of chunks by order
 
         for (int chunk_no = 0; chunk_no < number_of_chunks; chunk_no++) // Add chunks to array
             chunks.add(restore_channel.received_chunks.remove(Pair.create(file_id, chunk_no)));
 
+        Path path = Paths.get(file_path);
+
         try {
-            FileOutputStream stream = new FileOutputStream(file_path);
+            AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(path, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+            ByteBuffer buffer = ByteBuffer.allocate(chunks.size() * Storage.MAX_CHUNK_SIZE);
 
             for (byte[] chunk : chunks)
-                stream.write(chunk);
+                buffer.put(chunk);
 
-            stream.flush();
-            stream.close();
+            buffer.flip();
+
+            Future<Integer> operation = fileChannel.write(buffer, 0);
+            buffer.clear();
+
+            operation.get();
+
             return true;
         }
-        catch (IOException e) {
+
+        catch (IOException | ExecutionException | InterruptedException e) {
             e.printStackTrace();
+
             return false;
         }
     }
